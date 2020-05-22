efac7f9c8118e9183a03fc8321783748d76c6e4b
molina25 <molinadennis246@gmail.com>
2020-05-22 14:55:35 -0600

    primera version con git

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1> prueba de ejercicios  </h1>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae dolor ducimus, facilis consequatur iste, obcaecati amet sequi voluptate ipsum non quisquam eveniet similique, porro aperiam tenetur. Voluptates ex consectetur voluptas?
+</body>
+</html>
